3a9fc675990e00c803ce2007096895dbbc9144f4
Grace Odongo <graceakinyi024@gmail.com>
2025-08-30 21:07:55 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let left = dodger.style.left.replace("px", "");
+  left = parseInt(left, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left= dodger.style.left.replace("px", "");
+ left = parseInt(left, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
